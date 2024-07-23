f32526bbc285784fa4a1c614d9084e72f3b06011
Scott Johnstone <scottajohnstone@gmail.com>
2024-07-23 16:46:08 -0500

    fixed and tested all current endpoints

--- app.js
+++ app.js
@@ -72,34 +72,36 @@ app.post('/api/party', loginRequired, async(req, res) => {
 app.put('/api/party/:pcId', loginRequired, async (req, res) => {
     const { pcId } = req.params;
     const { pcImg, pcName, pcRace, pcClass, pcLevel, pcBackstory, pcAllies, pcGoals, pcExtras } = req.body;
-
-    
-    const index = Character.findIndex((item) => item.pcId === +(pcId));
-    if (index === -1) {
+ 
+    const editPC = await Character.findOne({ where: { pcId: +pcId}});
+    if (editPC.pcId !== +pcId) {
         res.status(404).json({ error: `Item with ID ${pcId} not found!`});
     } else {
-        const item = PC_DATA[index];
-        
-        // Only update the values that are provided in req.body
-        item.pcImg = pcImg || item.pcImg;
-        item.pcName = pcName || item.pcName;
-        item.pcRace = pcRace || item.pcRace;
-        item.pcClass = pcClass || item.pcClass;
-        item.pcLevel = +(pcLevel) || item.pcLevel;
-        
-        res.json(item);
+
+            // Only update the values that are provided in req.body
+            editPC.pcImg = pcImg || editPC.pcImg,
+            editPC.pcName = pcName || editPC.pcName,
+            editPC.pcRace = pcRace || editPC.pcRace,
+            editPC.pcClass = pcClass || editPC.pcClass,
+            editPC.pcLevel = +(pcLevel) || editPC.pcLevel,
+            editPC.pcBackstory = pcBackstory || editPC.pcBackstory,
+            editPC.pcAllies = pcAllies || editPC.pcAllies,
+            editPC.pcGoals = pcGoals || editPC.pcGoals,
+            editPC.pcExtras = pcExtras || editPC.pcExtras,
+            await editPC.save()
+        res.json(editPC);
     }
 });
 
 app.delete('/api/party/:pcId/delete', loginRequired, async (req, res) => {
     const { pcId } = req.params;
     
-    const index = Character.findIndex((item) => item.pcId === +(pcId));
-    if (index === -1) {
+    const deletePC = await Character.findOne({ where: { pcId: +pcId}});
+    if (deletePC.pcId !== +pcId) {
         res.status(404).json({ error: `Item with ID ${pcId} not found!`});
     } else {
-        Character.splice(index, 1);
-        res.json({ pcId: +(pcId) });
+        await deletePC.destroy()
+        res.json(deletePC);
     }
 });
 
@@ -108,45 +110,56 @@ app.get('/api/session_notes', loginRequired, async (req, res) => {
     res.json(allNotes);
 });
 
+app.get('/api/session_notes/:sesId', loginRequired, async (req, res) => {
+    const { sesId } = req.params;
+    const findSession = await SessionNote.findByPk(sesId);
+    res.json(findSession);
+  });
+
+  app.post('/api/session_notes', loginRequired, async (req, res) => {
+    const { sesId, sesNumber, sesDate, sesPartyLvl, sesNotes } = req.body;
+    
+    const newSes = await SessionNote.create({
+        // if no value is provided in req.body, use default values
+        sesId: sesId,
+        sesNumber: sesNumber || '',
+        sesDate: sesDate,
+        sesPartyLvl: +(sesPartyLvl) || 0,
+        sesNotes: sesNotes || '',
+
+    });
+    res.json(newSes);
+});
+
+app.put('/api/session_notes/:sesId', loginRequired, async (req, res) => {
+    const { sesId } = req.params;
+    const { sesNumber, sesDate, sesPartyLvl, sesNotes } = req.body;
+ 
+    const editSes = await SessionNote.findOne({ where: { sesId: +sesId}});
+    if (editSes.sesId !== +sesId) {
+        res.status(404).json({ error: `Session Notes with ID ${sesId} not found!`});
+    } else {
+
+            // Only update the values that are provided in req.body
+            editSes.sesNumber = sesNumber || editSes.sesNumber,
+            editSes.sesDate = sesDate || editSes.sesDate,
+            editSes.sesPartyLvl = sesPartyLvl || editSes.sesPartyLvl,
+            editSes.sesNotes = sesNotes || editSes.sesNotes,
+            
+            await editSes.save()
+        res.json(editSes);
+    }
+});
 app.delete('/api/session_notes/:sesId/delete', loginRequired, async (req, res) => {
     const { sesId } = req.params;
     
-    const index = SessionNote.findIndex((item) => item.sesId === +(sesId));
-    if (index === -1) {
-        res.status(404).json({ error: `Item with ID ${sesId} not found!`});
+    const deleteSes = await SessionNote.findOne({ where: { sesId: +sesId}});
+    if (deleteSes.sesId !== +sesId) {
+        res.status(404).json({ error: `Session Notes with ID ${sesId} not found!`});
     } else {
-        SessionNote.splice(index, 1);
-        res.json({ sesId: +(sesId) });
+        await deleteSes.destroy()
+        res.json(deleteSes);
     }
 });
 
 ViteExpress.listen(app, port, () => console.log(`Server is listening on http://localhost:${port}`));
-
-
-// const USER_DATA = [
-//     { userId: 0, email: 'scott@testemail.com', password: 'password', userName: 'Scott', isDM: true },
-//     { userId: 1, email: 'player@testemail.com', password: 'password', userName: 'Player', isDM: false },
-// ]
-
-// const PC_DATA = [
-//     { pcId: 0, pcName: 'Cawlin', pcImg: 'cawlin.png', pcRace: 'Kenku', pcClass: 'Rogue | Warlock - Hexblade', pcLevel: 7,
-//         pcBackstory: 'Lots of words here',
-//         pcAllies: 'Friendly Person', pcGoals: 'Do things, and stuff', pcExtras: 'Random stuff here' },
-//     { pcId: 1, pcName: 'Garfield Beast Boy Logan', pcImg: 'beast_boy.png', pcRace: 'Goblin', pcClass: 'Druid - Moon', pcLevel: 7,
-//         pcBackstory: 'Lots of words here',
-//         pcAllies: 'Friendly Person', pcGoals: 'Do things, and stuff', pcExtras: 'Random stuff here' },
-//     { pcId: 2, pcName: 'Bender Rodriguez', pcImg: 'bender.png', pcRace: 'Warforged', pcClass: 'Artificer - Armorer', pcLevel: 7,
-//         pcBackstory: 'Lots of words here',
-//         pcAllies: 'Friendly Person', pcGoals: 'Do things, and stuff', pcExtras: 'Random stuff here' },
-//     { pcId: 3, pcName: 'Ahsoka Tano', pcImg: 'ahsoka.png', pcRace: 'Hobgoblin', pcClass: 'Fighter - Psi Warrior', pcLevel: 7,
-//         pcBackstory: 'Lots of words here',
-//         pcAllies: 'Friendly Person', pcGoals: 'Do things, and stuff', pcExtras: 'Random stuff here' },
-// ]
-
-// const SESSION_DATA = [
-//     { sesId: 0, sesNumber: 0, sesDate: '2024-01-01', sesPartyLevel: 0, sesNotes: 'Session 0 notes here' },
-//     { sesId: 1, sesNumber: 1, sesDate: '2024-01-08', sesPartyLevel: 1, sesNotes: 'Session 1 notes here' },
-//     { sesId: 2, sesNumber: 2, sesDate: '2024-01-15', sesPartyLevel: 2, sesNotes: 'Session 2 notes here' },
-//     { sesId: 3, sesNumber: 3, sesDate: '2024-01-22', sesPartyLevel: 2, sesNotes: 'Session 3 notes here' },
-//     { sesId: 4, sesNumber: 4, sesDate: '2024-01-29', sesPartyLevel: 3, sesNotes: 'Session 4 notes here' }
-// ]
